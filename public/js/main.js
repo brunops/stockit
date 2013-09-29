818216c264f0b8c2380a99f058811d6a2c330633
Bruno Sanches <brunopsanches@gmail.com>
2013-09-28 19:40:35 -0700

    Prevent empty values to be posted

--- public/js/main.js
+++ public/js/main.js
@@ -5,9 +5,14 @@
   });
 
   function addSearchBehavior() {
+    var inputEl = $('#search_input');
     $('#search').click(function() {
+      if (!isInputValid(inputEl.val())) {
+        return false;
+      }
+
       var postData = {
-        name: this.value
+        name: inputEl.val()
       }
 
       $.post('/stocks', postData, function(data) {
@@ -20,5 +25,9 @@
 
   function updateSearchResult(data) {
     $('#search_result').html(data);
+  }
+
+  function isInputValid(value) {
+    return $.trim(value) !== "";
   }
 })(jQuery);
