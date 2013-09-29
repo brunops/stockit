e007148b16a170a64b0a7a166c855d09029ab2df
maxdavila <adavila0711@gmail.com>
2013-09-29 07:55:27 -0700

    add next day results

--- app.rb
+++ app.rb
@@ -1,13 +1,14 @@
 require "sinatra"
 require "net/http"
 require "json"
+require 'pry'
 
 get '/' do
   erb :home
 end
 
 post '/stocks' do
-  JSON.dump(closing_percentage_changes(params[:name]))
+  JSON.dump(get_next_day_data(params[:name]))
 end
 
 helpers do
@@ -29,10 +30,27 @@ helpers do
     "select * from yahoo.finance.historicaldata where symbol = '#{ticker}' and startDate = '#{start_date}' and endDate = '#{end_date}'"
   end
 
-  def matching_dates(stock)
+  
+  def get_next_day_data(stock)
+    all_matching_dates = get_all_matching_dates(stock)
+    up_days_count = 0
+    all_matching_dates.each do |current_day_data|
+      current_day_index = @closing_percentage_changes.index(current_day_data)
+      if (current_day_index + 1) < @closing_percentage_changes.length 
+        next_day_data = @closing_percentage_changes[current_day_index + 1]
+        up_days_count+= 1 if next_day_data["change"] > 0
+      end
+    end
+     get_highest_probability(up_days_count,(all_matching_dates.length-1))
+  end
+
+  def get_highest_probability(up_days, total_days)
+  end
+  
+  def get_all_matching_dates(stock)
     percentages_array = closing_percentage_changes(stock)
-    percentage_to_match = percentages_array.first['change']
-    percentage_range_to_match = ((percentage_to_match-0.05)..(percentage_to_match+0.5))
+    percentage_to_match = percentages_array.last['change']
+    percentage_range_to_match = ((percentage_to_match-0.05)..(percentage_to_match+0.05))
     percentages_array.select { |current_percentage| percentage_range_to_match.include? current_percentage["change"] }
   end
 
@@ -45,7 +63,7 @@ helpers do
         'change' => daily_closing_percentage_change(current['Adj_Close'], previous['Adj_Close'])
       }
     end
-    changed_percentage.reverse
+    @closing_percentage_changes = changed_percentage
   end
 
   def daily_closing_percentage_change(today_value, yesterday_value)
